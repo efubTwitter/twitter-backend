81b67067a18f375ae2d0bb0c464c88f0ccfd0db7
YEJI <yeji.lee.kim@gmail.com>
2023-05-21 15:42:33 +0900

    feat: user의 Response DTO에 id 추가

--- src/main/java/twitter/domain/user/dto/UserResponseDto.java
+++ src/main/java/twitter/domain/user/dto/UserResponseDto.java
@@ -8,17 +8,19 @@ import java.time.LocalDateTime;
 
 /*
 {
+	"userId" : "chlove_u",
     "profilePhoto": "",
     "headerPhoto": "",
-    "name": "퍼비",
-    "bio" : "안녕하세요!",
-    "joinedDate": "2021-07-24T01:33:40:000000"
+    "name": "문화재청",
+    "bio" : "경산토기를 쓴 문화재청 공식 트위터입니다.",
+    "joinedDate": "2011-03-24T01:33:40:000000"
 }
  */
 
 @Getter
 @NoArgsConstructor
 public class UserResponseDto {
+    private String userId;
     private String profilePhoto;
     private String headerPhoto;
     private String name;
@@ -26,6 +28,7 @@ public class UserResponseDto {
     private LocalDateTime joinedDate;
 
     public UserResponseDto(User user){
+        this.userId = user.getUserId();
         this.profilePhoto = user.getProfilePhoto();
         this.headerPhoto= user.getHeaderPhoto();
         this.name= user.getName();
